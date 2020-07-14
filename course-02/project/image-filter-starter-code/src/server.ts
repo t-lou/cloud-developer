b5da268a669afb095efbaa82405b0f07318c4fe8
Tongxi Lou <anotherlasse@gmail.com>
2020-07-15 00:44:48 +0200

    beautify

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -1,6 +1,6 @@
 import express, { Router, Request, Response } from 'express';
 import bodyParser from 'body-parser';
-import {filterImageFromURL, deleteLocalFiles} from './util/util';
+import { filterImageFromURL, deleteLocalFiles } from './util/util';
 
 (async () => {
 
@@ -31,8 +31,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   app.get('/filteredimage/', async (req: Request, res: Response) => {
     let { image_url } = req.query;
 
-    if (!image_url)
-    {
+    if (!image_url) {
       return res.status(400).send(`image_url is required`);
     }
 
@@ -40,12 +39,10 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
     filtered.then(filtered_path => {
       res.status(200).sendFile(filtered_path, err => {
-        if (err)
-        {
+        if (err) {
           res.status(406).send(`Cannot send file!`);
         }
-        else
-        {
+        else {
           deleteLocalFiles([filtered_path]);
         }
       });
@@ -56,14 +53,14 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
   // Root Endpoint
   // Displays a simple message to the user
-  app.get( "/", async ( req, res ) => {
+  app.get("/", async (req, res) => {
     res.send("try GET /filteredimage?image_url={{}}")
-  } );
+  });
 
 
   // Start the Server
-  app.listen( port, () => {
-      console.log( `server running http://localhost:${ port }` );
-      console.log( `press CTRL+C to stop server` );
-  } );
+  app.listen(port, () => {
+    console.log(`server running http://localhost:${port}`);
+    console.log(`press CTRL+C to stop server`);
+  });
 })();
